f6ada5d0f2d5397cf34075f22e658d4707ce9b6e
drrr-py <noob-pg-drunk@startmail.com>
2019-07-24 19:02:35 +0900

    [update] ファイルを変更できていなかったので修正

--- public/js/header.js
+++ public/js/header.js
@@ -1,13 +1,43 @@
-let socketio = io();
-$(function() {
-  $("#message_form").submit(function() {
-    let msg = $("#input_msg");
-    socketio.emit("message", msg.val());
-    msg.val("");
-    return false;
-  });
+let socket = io();
 
-  socketio.on("message", function(msg) {
-    $("#messages").append($("<li>").text(msg));
+window.addEventListener("load", () => {
+  let wrapper = document.getElementById("wrapper");
+  let header = document.createElement("header");
+  wrapper.appendChild(header);
+
+  let label = document.createElement("label");
+  label.textContent = "名前：";
+  let input = document.createElement("input");
+  input.id = "input_name";
+  input.autocomplete = false;
+  label.appendChild(input);
+
+  let button = document.createElement("button");
+  button.type = "button";
+  button.id = "name_send_button";
+  button.textContent = "決 定"
+
+  header.appendChild(label);
+  header.appendChild(button);
+
+  let hr = document.createElement("hr");
+  wrapper.appendChild(hr);
+});
+
+window.addEventListener("load", () => {
+  document.getElementById("name_send_button").addEventListener("click", () => {
+    let name = document.getElementById("input_name").value;
+
+//    // セッションストレージにユーザーネームを保存
+//    // 今は必要性を感じないので無効
+//    sessionStorage.setItem("name", name);
+
+    socket.emit("user_name", name);
   });
 });
+
+//// 同一ipで切断された場合
+//// 同一ip切断を無効化しているためこちらも無効
+//socket.on("ip_alert", (msg) => {
+//  alert(msg);
+//});
