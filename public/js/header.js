9959e5a6a1474bacca516244051f6b99b9b7a4fc
S-Del <noob-pg-drunk@startmail.com>
2019-07-27 03:56:32 +0900

    Revert "[upgrade / update / clean] ラウンジチャット機能の追加とリファクタリング等"
    This reverts commit 63c9beed866b5a15db5078a88bdc67f62645e62c.

--- public/js/header.js
+++ public/js/header.js
@@ -1,40 +1,20 @@
 /**
  * 要素生成
- *
- * <header>
- *   <label>
- *     名前：<input id="input_name">
- *   </label>
- *   <label>
- *     トリップ:<input id="input_trip">
- *   </label>
- *   <button type="button" id="name_send_button">決 定</button>
- *
- *   <span>
- *     現在の状態 - 
- *     名前: <span id="crnt_name"></span>
- *     ID: <span id="crnt_id"></span>
- *   </span>
- * </header>
  */
 window.addEventListener("load", () => {
   let wrapper = document.getElementById("wrapper");
   let header = document.createElement("header");
   wrapper.appendChild(header);
 
-  // 名前入力部
   let name_label = document.createElement("label");
   name_label.textContent = "名前:";
-  header.appendChild(name_label);
   let input_name = document.createElement("input");
   input_name.id = "input_name";
   input_name.maxlength = "15";
   name_label.appendChild(input_name);
 
-  // トリップ入力部
   let trip_label = document.createElement("label");
   trip_label.textContent = "トリップ:";
-  header.appendChild(trip_label);
   let input_trip = document.createElement("input");
   input_trip.id = "input_trip";
   input_trip.maxlength = "8";
@@ -44,30 +24,38 @@ window.addEventListener("load", () => {
   button.type = "button";
   button.id = "name_send_button";
   button.textContent = "決 定"
-  header.appendChild(button);
 
-  // 状態表示部
   let crnt_status = document.createElement("span");
-  crnt_status.textContent = "現在の状態 - 名前: ";
-  header.appendChild(crnt_status);
+  crnt_status.textContent = "現在の状態 - ";
 
+  let crnt_name_label = document.createElement("span");
+  crnt_name_label.textContent = "名前:";
+  crnt_status.appendChild(crnt_name_label);
   let crnt_name = document.createElement("span");
   crnt_name.id = "crnt_name";
   crnt_name.textContent = "";
   crnt_status.appendChild(crnt_name);
 
-  let crnt_id_label = document.createTextNode(" - ID: ")
+  let crnt_id_label = document.createElement("span");
+  crnt_id_label.textContent = "ID:";
   crnt_status.appendChild(crnt_id_label);
-
   let crnt_id = document.createElement("span");
   crnt_id.id = "crnt_id";
   crnt_id.textContent = "";
   crnt_status.appendChild(crnt_id);
+
+  header.appendChild(name_label);
+  header.appendChild(trip_label);
+  header.appendChild(button);
+  header.appendChild(crnt_status);
+
+  let hr = document.createElement("hr");
+  wrapper.appendChild(hr);
 });
 
 
 /**
- * 要素やsocketのイベントを設定
+ * 要素のイベントを設定
  */
 window.addEventListener("load", () => {
   // 名前入力欄でのエンターキー入力で決定する
@@ -95,15 +83,16 @@ window.addEventListener("load", () => {
     socket.emit("change_name", name, trip);
   });
 
+
   // 名前やIDが変更された場合に呼び出されるsocketイベント
   socket.on("update_user_info", (user_info) => {
     document.getElementById("crnt_name").textContent = user_info.name;
     document.getElementById("crnt_id").textContent = user_info.id;
   });
 
-//  // 同一ipで切断された場合
-//  // 同一ip切断を無効化しているためこちらも無効
-//  socket.on("ip_alert", (msg) => {
-//    alert(msg);
-//  });
+//// 同一ipで切断された場合
+//// 同一ip切断を無効化しているためこちらも無効
+//socket.on("ip_alert", (msg) => {
+//  alert(msg);
+//});
 });
