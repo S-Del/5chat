f72bf2c17dbb55b199e9f92893ac69509efd07fc
S-Del <noob-pg-drunk@startmail.com>
2019-07-29 02:37:22 +0900

    [upgrade / clean] 作成した部屋を一覧へ反映と一覧を更新する機能の追加。
    helmetモジュールの追加で不要になった部分の削除等、リファクタリングも行った。
    main.jsも参照。

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
+"use strict";
+
 let crypto = require("crypto");
 let express = require("express");
+let helmet = require("helmet");
 let app = express();
 let server = require("http").Server(app);
 let io = require("socket.io")(server, { cookie: false });
 const PORT = process.env.PORT || 8080;
+
+app.use(helmet());
+app.use(express.static("public"));
+app.use((req, res) => {
+  res.sendStatus(404);
+});
+
+server.listen(PORT, () => {
+  console.log("Server is Listening. Port: " + PORT);
+});
 
 let users = {};
 let rooms = {};
@@ -12,20 +25,10 @@ let lounge = {
   res_no: 0
 };
 
-server.listen(PORT, () => {
-  console.log("Server is Listening. Port: " + PORT);
-});
-
-app.disable('x-powered-by');
-
-app.use(express.static("public"));
-app.use((req, res) => {
-  res.sendStatus(404);
-});
-
 io.on("connection", (socket) => {
   console.log("---------- " + socket.id + " Connected ----------");
   init_user_info(socket);
+  socket.emit("update_room_list", rooms);
   put_users();
 
   socket.on("disconnect", (reason) => {
@@ -40,9 +43,52 @@ io.on("connection", (socket) => {
     put_users();
   });
 
+  socket.on("relord_list", () => {
+    socket.emit("update_room_list", rooms);
+  });
+
+  socket.on("create_new_room", (new_room_info) => {
+    if (is_blank(new_room_info.input_room_name)) {
+      return;
+    }
+
+    if (is_blank(new_room_info.input_room_description)) {
+      new_room_info.input_room_description = "説明なし";
+    }
+
+    let new_room = {
+      name: new_room_info.input_room_name,
+      desc: new_room_info.input_room_description.slice(0, 60),
+      owner: socket.id,
+      res_no: 0,
+      power: 0,
+      users: {}
+    };
+    new_room.users[socket.id] = {
+      name: users[socket.id].name,
+      id: users[socket.id].id,
+      power: users[socket.id].power
+    };
+
+    let sha512 = crypto.createHash("sha512");
+    // 要変更(ipと部屋名だけではだめ)
+    sha512.update(users[socket.id].ip + new_room.name);
+    let room_id = sha512.digest("hex");
+
+    rooms[room_id] = new_room;
+
+    socket.join(room_id);
+//    console.log(rooms[room_id].users);
+  });
+
+  socket.on("join_room", (room_id) => {
+    // roomが存在するかを確認
+    console.log(rooms[room_id]);
+  });
+
   socket.on("send_to_lounge", (message_text) => {
     message_text = message_text.slice(0, 60);
-    if (!validate_message(message_text)) {
+    if (is_blank(message_text)) {
       return;
     }
 
@@ -65,46 +111,32 @@ io.on("connection", (socket) => {
 
 
 /**
- * ユーザーが入力した文字列を検証する
- *
- * 入力に問題が無ければtrue、
- * 空文字列・空白の連続などであった場合はfalseを返す。
- */
-function validate_message(message_text) {
-  if (!message_text) {
-    return false;
-  }
-
-  let regex = /^[\s\n\r\t]+$/;
-  if (regex.test(message_text)) {
-    return false;
-  }
-
-  return true;
-}
-
-
-/**
  * 接続が確立した時のユーザー情報初期化用
  * ユーザー名とIDを設定してクライアントに送信する
- * 設定したユーザー情報はusers[]に保存される
+ * 設定したユーザー情報はusers{}に保存される
  */
 function init_user_info(socket) {
-  let connect_user = {};
-  connect_user.name = "名無しさん";
+  let name = "名無しさん";
 
   // 取得したipアドレスが "Pv4射影IPv6アドレス" 形式の場合はIPv6部分を取り除く
-  connect_user.ip = socket.handshake.address;
-  let idx = connect_user.ip.lastIndexOf(":");
+  let ip = socket.handshake.address;
+  let idx = ip.lastIndexOf(":");
   if (idx != -1) {
-    connect_user.ip = connect_user.ip.slice(idx + 1);
+    ip = ip.slice(idx + 1);
   }
 
   let sha512 = crypto.createHash("sha512");
-  sha512.update(connect_user.ip);
-  connect_user.id = sha512.digest("base64").slice(0, 10);
+  sha512.update(ip);
+  let id = sha512.digest("base64").slice(0, 10);
 
-  users[socket.id] = connect_user;
+  let new_user = {
+    name: name,
+    ip: ip,
+    id: id,
+    power: 0
+  };
+
+  users[socket.id] = new_user;
   update_user_info(socket);
 }
 
@@ -122,11 +154,39 @@ function update_user_info(socket) {
 
 
 /**
+ * ユーザーの入力が空であるかを検証する
+ *
+ * ユーザーが入力した文字列が
+ * ・空文字("")
+ * ・null
+ * ・undefined
+ * ・空白のみ("  "等)
+ * かを検証し、空文字列・空白の連続などであった場合はtrueを返す。
+ * そうで無ければfalseを返す。
+ *
+ * @param {string} user_input: ユーザーが入力した文字列
+ * @return {boolean} 問題があればtrue、無ければfalse
+ */
+function is_blank(user_input) {
+  if (!user_input) {
+    return true;
+  }
+
+  let regex = /^[\s\n\r\t]+$/;
+  if (regex.test(user_input)) {
+    return true;
+  }
+
+  return false;
+}
+
+
+/**
  * ユーザー名変更用
  * トリップが入力されている場合は名前に付加する
  */
 function change_name(name, trip) {
-  if (!name) {
+  if (is_blank(name)) {
     name = "名無しさん";
   }
   name = name.slice(0, 16).replace(/◆/g, "■");
