d8412c924bcdf1f7b4e14fd2cf440ec9b5bbf2f2
S-Del <noob-pg-drunk@startmail.com>
2019-07-30 18:50:41 +0900

    [upgrade / change] ラウンジの仕様変更とroomから切断ユーザーの情報を削除する機能を追加

--- rooms.js
+++ rooms.js
@@ -13,9 +13,37 @@ exports.map = rooms;
  */
 let lounge = {
   part: 1,
-  res_no: 0
+  messages: []
 };
 exports.lounge = lounge;
+
+
+/**
+ * socket.idで指定されたユーザーを全ての部屋から削除する
+ */
+let delete_user(socket_id) {
+  for (let room_id in rooms) {
+    if (!rooms[room_id].users[socket_id]) {
+      continue;
+    }
+    delete rooms.map[room_id].users[socket_id];
+  }
+}
+exports.delete_user = delete_user;
+
+
+/**
+ * ユーザ情報、メッセージを保存し、ラウンジ情報を更新する。
+ */
+let update_lounge = (user_info) => {
+  if (lounge.messages.length > 1000) {
+    lounge.messages = [];
+    rooms.lounge.part++;
+  }
+
+  lounge.messages.push(user_info);
+}
+exports.update_lounge = update_lounge;
 
 
 /**
